eabe2a99a7413a903ee16b92bdb6e9e7f15cff7c
Seshpenguin <seshan10@me.com>
2017-02-18 12:57:26 -0500

    Hai

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,12 @@
 */
 console.log('Starting GitRTC backend server...');
 var io = require('socket.io')(80);
+var name = {}; 
 
 io.on('connection', function (socket) {
   socket.on('helloWorld', function (data, fn) {
     console.log(data)
+    console.log(socket.id)
     fn('helloClient');
   });
 });
